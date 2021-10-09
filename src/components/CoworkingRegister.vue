bdc5a7df81feb18131ac77dc5f59a5b5b836f6e5
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-09 15:14:54 -0300

    Create coworking and get all.

--- src/components/CoworkingRegister.vue
+++ src/components/CoworkingRegister.vue
@@ -1,9 +1,9 @@
 <template>
   <h1 class="heading">Register Coworking</h1>
-    <form class="form">
-        <label>Coworking Name</label>
-        <input type="text" placeholder="Coworking name"/>
-      <input class="form__btn" type="submit">
+    <form action="http://localhost:3000/coworking" method="POST" class="form">
+        <label for="name">Coworking Name</label>
+        <input type="text" id="name" name="name" placeholder="Coworking name"/>
+      <button class="form__btn" type="submit">Submit</button>
     </form>
 </template>
 
