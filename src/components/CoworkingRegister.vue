c1502877e2b53828162e095b007aa224a8068978
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-15 17:38:16 -0300

    Set create and retrieve all from CRrUD.

--- src/components/CoworkingRegister.vue
+++ src/components/CoworkingRegister.vue
@@ -1,19 +1,71 @@
 <template>
   <h1 class="heading">Register Coworking</h1>
-    <form action="http://localhost:3000/coworking" method="POST" class="form">
-        <label for="name">Coworking Name</label>
-        <input type="text" id="name" name="name" placeholder="Coworking name"/>
-      <button class="form__btn" type="submit">Submit</button>
-    </form>
+  <form
+      class="form"
+  >
+      <label for="name">Coworking Name</label>
+      <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Coworking name"
+          v-model="coworking.name"
+      />
+      <button
+          class="form__btn"
+          type="button"
+          v-on:click="submit"
+      >Submit
+      </button>
+  </form>
+
+  <ul>
+    <li v-for="i in all" :key="i">{{ i.name }}</li>
+  </ul>
 </template>
 
 <script>
+import {ref} from 'vue';
+import axios from 'axios';
 
 export default {
   name: 'RegisterCoworking',
-  data: () => ({
-      name: String,
-  }),
+  data () {
+    return {
+      coworking: {
+        name: ''
+      },
+    }
+  },
+  methods: {
+    submit() {
+      axios.post("http://localhost:3000/coworking", this.coworking);
+      location.reload();
+    }
+  },
+  setup() {
+    /**
+     * Retrieve All
+     */
+    const all = ref({});
+
+    fetch('http://localhost:3000/coworkings')
+        .then(response => response.json())
+        .then(data => all.value = data);
+
+    /**
+     * Create
+     */
+    // const postData = () => {
+    //   axios.post("http://localhost:3000/coworking", this.coworking.name)
+    // }
+    // postData();
+    // console.log('postData', postData());
+
+    return {
+      all,
+    }
+  }
 }
 </script>
 
