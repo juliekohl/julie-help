d257e7202794db5e6fe2ceeaa1b70452c78e766f
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-25 18:14:14 -0300

    Create route retrieve single teams.vue.

--- src/pages/TeamsView.vue
+++ src/pages/TeamsView.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>Teams View</h1>
+  <h1>Team Info</h1>
+  <div class="info">
+    <span>Name: {{teamUser.name}}</span>
+    <span>Email: {{teamUser.email}}</span>
+  </div>
 </template>
 
 <script lang="ts">
+import {ref} from "vue";
+import {useRouter} from "vue-router";
+
 export default {
-  name: 'TeamsView'
+  name: 'TeamsView',
+
+  setup() {
+    const router = useRouter();
+    const teamUser = ref({id: null, name: '', email: ''});
+    const teamUserId = Number(router.currentRoute.value.params.id);
+
+    fetch(`http://localhost:3000/teams/${teamUserId}`)
+        .then(response => response.json())
+        .then(data => {teamUser.value = data});
+
+    return {
+      teamUser,
+    }
+  },
 }
 </script>
+
+<style scoped>
+.info {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+</style>
