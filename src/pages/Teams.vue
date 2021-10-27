2018dbc8beeb8b34ea774c18bae08b893479fcef
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-27 12:25:37 -0300

    Bugfix.

--- src/pages/Teams.vue
+++ src/pages/Teams.vue
@@ -18,7 +18,7 @@ export default {
         .then(response => response.json())
         .then(data => {all.value = data});
 
-    const handleListItem = (e: Event): void => {
+    const handleListItem = (e: any): void => {
       teamUser.value.id = e.target.getAttribute('data-id');
       teamUser.value.name = e.target.innerHTML;
     }
