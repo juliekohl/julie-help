0911d0a146fc5fd2e4da8811585066a7a33769ea
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-28 10:06:49 -0300

    Add router link to TeamsView.vue

--- src/pages/Teams.vue
+++ src/pages/Teams.vue
@@ -1,15 +1,25 @@
 <template>
   <h1>Teams</h1>
   <ul>
-    <li v-for="i in all" :key="i" :data-id="i.id" @click="handleListItem">{{ i.name }}</li>
+    <li
+        v-for="i in all"
+        :key="i"
+        :data-id="i.id"
+        @click="handleListItem"
+    >
+      <router-link
+          :to="{ name: 'TeamsView', params: { id: i.id }}"
+      >{{ i.name }}</router-link>
+    </li>
   </ul>
 </template>
 
 <script lang="ts">
-import {ref} from "vue";
+import {defineComponent, ref} from "vue";
 
-export default {
+export default defineComponent( {
   name: 'Teams',
+
   setup() {
     const all = ref({});
     const teamUser = ref({id: null, name: ''});
@@ -21,6 +31,8 @@ export default {
     const handleListItem = (e: any): void => {
       teamUser.value.id = e.target.getAttribute('data-id');
       teamUser.value.name = e.target.innerHTML;
+      const teamUserId = teamUser.value.id;
+      console.log('id', teamUserId);
     }
 
     return {
@@ -28,5 +40,10 @@ export default {
       handleListItem
     }
   },
-}
+})
 </script>
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
