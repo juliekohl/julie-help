a0255a3a475f3dae25e613d568bc7a2a1529e1fb
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-10-28 10:09:32 -0300

    Add router link to TeamsEdit.vue

--- src/pages/TeamsView.vue
+++ src/pages/TeamsView.vue
@@ -3,14 +3,24 @@
   <div class="info">
     <span>Name: {{teamUser.name}}</span>
     <span>Email: {{teamUser.email}}</span>
+    <button
+        class="form__btn"
+        type="button"
+        v-on:click="handleEdit"
+    >
+      <router-link
+          :to="{ name: 'TeamsEdit', params: { id: teamUserId }}"
+      >Edit</router-link>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import {ref} from "vue";
+import {defineComponent, ref} from "vue";
 import {useRouter} from "vue-router";
+import axios from "axios";
 
-export default {
+export default defineComponent( {
   name: 'TeamsView',
   setup() {
     const router = useRouter();
@@ -23,9 +33,16 @@ export default {
 
     return {
       teamUser,
+      teamUserId
     }
   },
-}
+  methods: {
+    handleEdit(): void {
+      axios.post(`http://localhost:3000/teams/${this.teamUserId}`, this.teamUser);
+      location.reload();
+    }
+  },
+})
 </script>
 
 <style scoped>
