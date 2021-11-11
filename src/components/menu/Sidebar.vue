63003e49c7276bc564b076f99fd09fac6b95653e
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-11 16:47:48 -0300

    Add sidebar, WIP.

---
+++ src/components/menu/Sidebar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="sidebar">
+    <div
+        class="sidebar-backdrop"
+        @click="closeSidebarPanel"
+        v-if="isPanelOpen"
+    ></div>
+    <transition name="slide">
+      <div
+          v-if="isPanelOpen"
+          class="sidebar-panel"
+      >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+// import { store, mutations } from "@/store";
+
+export default defineComponent ({
+  name: 'Sidebar',
+  data: () => ({
+    isPanelOpen: true
+  }),
+  methods: {
+    closeSidebarPanel() {
+      this.isPanelOpen = false
+    }
+    // closeSidebarPanel: mutations.toggleNav
+  },
+  computed: {
+    isPanelOpen() {
+      return this.isPanelOpen
+      // return store.isNavOpen
+    }
+  }
+})
+</script>
+
+<style>
+.slide-enter-active,
+.slide-leave-active
+{
+  transition: transform 0.2s ease;
+}
+
+.slide-enter,
+.slide-leave-to {
+  transform: translateX(-100%);
+  transition: all 150ms ease-in 0s
+}
+
+.sidebar-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--color-muted);
+  cursor: pointer;
+}
+
+.sidebar-panel {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  width: 300px;
+  height: 100vh;
+  padding: 3rem 20px 2rem 20px;
+  overflow-y: auto;
+  background-color: var(--color-primary-100);
+}
+</style>
