0728d304a63bd1200a85722095f9b30263c0675f
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-11 18:33:19 -0300

    Fix.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-// import store from '@/store';
 // import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
 // import { ValidationObserver } from 'vee-validate';
 
 import index from './router/index';
 import '@/assets/scss/index.scss';
-import {store} from "@/store";
 
 createApp(App)
     .use(index)
-    .use(store)
     .mount('#app')
