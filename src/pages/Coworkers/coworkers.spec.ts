2cbef151406e333346e37682d45806ed484a702f
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-16 11:17:23 -0300

    Improve tests.

--- src/pages/Coworkers/coworkers.spec.ts
+++ src/pages/Coworkers/coworkers.spec.ts
@@ -9,4 +9,16 @@ describe('Coworkers', () => {
         const coworkers = wrapper.find('div.coworkers');
         expect(coworkers.exists()).toBe(true);
     })
+    it('should have Coworkers as H1', () => {
+        const wrapper = mount(Coworkers);
+
+        const h1cCoworkers = wrapper.find('h1');
+        expect(h1cCoworkers.text()).toContain('Coworkers');
+    })
+    it('should have add new coworker as button', () => {
+        const wrapper = mount(Coworkers);
+
+        const addNewCoworker = wrapper.find('button.form__btn');
+        expect(addNewCoworker.exists()).toBe(true);
+    })
 })
