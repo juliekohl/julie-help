efa025bddc2b42df01894f04c0f14c89630a6542
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-19 12:23:34 -0300

    Create editTeams.spec.ts test and EditTeams.vue HTML.

---
+++ src/components/teams/EditTeams.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="edit-teams" v-if="teamUserId">
+    <div class="edit-teams__header">
+      <h1 class="edit-teams__header--heading">{{teamUser.name}}</h1>
+      <button
+          class="edit-teams__header--btn-delete"
+          v-on:click="handleDelete"
+      >
+        Delete
+      </button>
+      <button
+          class="edit-teams__header--btn"
+          v-on:click="handleEdit"
+      >
+        Edit
+      </button>
+    </div>
+    <form class="edit-teams__form">
+      <label
+          class="edit-teams__form--label"
+          for="name"
+      >
+        Name
+      </label>
+      <input
+          class="edit-teams__form--input"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="user name edit"
+          v-model="teamUser.name"
+      />
+      <label
+          class="edit-teams__form--label"
+          for="password"
+      >
+        Password
+      </label>
+      <input
+          class="edit-teams__form--input"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="user password edit"
+          v-model="teamUser.password"
+      />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'EditTeams',
+}
+</script>
+<style lang="scss">
+
+</style>
