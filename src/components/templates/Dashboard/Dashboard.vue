82d1d9042b7dbe236d47ff705f25063dd1e51dcf
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-30 17:37:37 -0300

    WIP.

--- src/components/templates/Dashboard/Dashboard.vue
+++ src/components/templates/Dashboard/Dashboard.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>Dashboard</h1>
+  <RegisterOffices/>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import RegisterOffices from "@/components/templates/OfficesOld/Offices.vue";
 
 export default defineComponent( {
   name: 'Dashboard',
+  components: {RegisterOffices},
 })
 </script>
 
