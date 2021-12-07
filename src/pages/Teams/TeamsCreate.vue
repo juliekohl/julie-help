b86d47164a772be2d0828bf656ee23800be2a826
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-12-07 17:01:30 -0300

    Create TeamsCreate.vue.

--- src/pages/Teams/TeamsCreate.vue
+++ src/pages/Teams/TeamsCreate.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="coworkers-create">
+  <div class="teams-create">
     <Form
-        class="coworkers-create__form"
+        class="teams-create__form"
         @submit="onSubmit"
         :validation-schema="schema"
     >
-      <div class="coworkers-create__header">
-        <h1 class="coworkers-create__heading">Create Coworker</h1>
+      <div class="teams-create__header">
+        <h1 class="teams-create__heading">Create Team</h1>
         <button-unit
-            class="coworkers-create__button"
+            class="teams-create__button"
             color="purple"
         >
-          Add New Coworker
+          Add New Team
         </button-unit>
       </div>
 
       <Field
-          class="coworkers-create__input"
+          class="teams-create__input"
           name="coworking_id"
           :value="1"
           type="hidden"
       />
 
       <label
-          class="coworkers-create__label"
+          class="teams-create__label"
           for="name"
       >
         Name
       </label>
       <Field
-          class="coworkers-create__input"
+          class="teams-create__input"
           type="text"
           id="name"
           name="name"
@@ -37,13 +37,13 @@
       <ErrorMessage name="name" />
 
       <label
-          class="coworkers-create__label"
+          class="teams-create__label"
           for="email"
       >
         Email
       </label>
       <Field
-          class="coworkers-create__input"
+          class="teams-create__input"
           type="email"
           id="email"
           name="email"
@@ -51,13 +51,13 @@
       <ErrorMessage name="email" />
 
       <label
-          class="coworkers-create__label"
+          class="teams-create__label"
           for="password"
       >
         Password
       </label>
       <Field
-          class="coworkers-create__input"
+          class="teams-create__input"
           type="password"
           id="password"
           name="password"
@@ -75,7 +75,7 @@ import { Field, Form, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
 
 export default defineComponent({
-  name: 'CoworkersCreate',
+  name: 'TeamsCreate',
   components: {ButtonUnit, Field, Form, ErrorMessage},
   data() {
     const schema = yup.object({
@@ -89,15 +89,15 @@ export default defineComponent({
   },
   methods: {
     onSubmit(values: any) {
-      axios.post(`${process.env.VUE_APP_BACKEND_URL}/coworkers`, values);
-      this.$router.push({ name: 'CoworkersRetrieveAll' });
+      axios.post(`${process.env.VUE_APP_BACKEND_URL}/teams`, values);
+      this.$router.push({ name: 'TeamsRetrieveAll' });
     },
   },
 })
 </script>
 
 <style lang="scss">
-.coworkers-create {
+.teams-create {
   @include page-create();
 }
 </style>
