9d237566a764dad9fc648bbdd86855511dbbcf75
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-12-10 15:17:04 -0300

    Fix CoworkersCreate.spec.ts.

--- src/pages/Coworkers/CoworkersCreate.spec.ts
+++ src/pages/Coworkers/CoworkersCreate.spec.ts
@@ -1,7 +1,7 @@
 import {mount} from "@vue/test-utils";
-import CoworkersCreate from "@/components/templates/CoworkersOld/CoworkersCreate.vue";
+import CoworkersCreate from "@/pages/Coworkers/CoworkersCreate.vue";
 
-describe('New CoworkersOld', () => {
+describe('New Coworkers', () => {
     it('is an HTML tag div', () => {
         const wrapper = mount(CoworkersCreate);
 
@@ -12,35 +12,35 @@ describe('New CoworkersOld', () => {
     it('should have an H1 containing New Coworker', () => {
         const wrapper = mount(CoworkersCreate);
 
-        const h1 = wrapper.find('h1.coworkers-create__header--heading');
-        expect(h1.text()).toContain('New Coworker');
+        const h1 = wrapper.find('h1.coworkers-create__heading');
+        expect(h1.text()).toContain('Create Coworker');
     })
 
     it('should have add new coworker as button', () => {
         const wrapper = mount(CoworkersCreate);
 
-        const button = wrapper.find('button.coworkers-create__header--button');
+        const button = wrapper.find('button.coworkers-create__button');
         expect(button.exists()).toBe(true);
     })
 
     it('should have the form', () => {
         const wrapper = mount(CoworkersCreate);
 
-        const form = wrapper.find('form.coworkers-create__form');
+        const form = wrapper.find('Form.coworkers-create__form');
         expect(form.exists()).toBe(true);
     })
 
     it('should have label in the form', () => {
         const wrapper = mount(CoworkersCreate);
 
-        const label = wrapper.find('label.coworkers-create__form--label');
+        const label = wrapper.find('label.coworkers-create__label');
         expect(label.exists()).toBe(true);
     })
 
     it('should have input in the form', () => {
         const wrapper = mount(CoworkersCreate);
 
-        const input = wrapper.find('input.coworkers-create__form--input');
+        const input = wrapper.find('input.coworkers-create__input');
         expect(input.exists()).toBe(true);
     })
 })
