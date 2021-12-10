ea6c795c8378d77914c6659800167fe730f2d878
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-12-10 15:17:33 -0300

    Fix CoworkersUpdate.spec.ts.

--- src/pages/Coworkers/CoworkersUpdate.spec.ts
+++ src/pages/Coworkers/CoworkersUpdate.spec.ts
@@ -1,5 +1,5 @@
 import {mount} from "@vue/test-utils";
-import CoworkersUpdate from "@/components/templates/CoworkersOld/CoworkersUpdate.vue";
+import CoworkersUpdate from "@/pages/Coworkers/CoworkersUpdate.vue";
 
 jest.mock('vue-router', () => {
     return {
@@ -26,14 +26,14 @@ describe('CoworkersOld Update', () => {
     it('should have an H1', () => {
         const wrapper = mount(CoworkersUpdate);
 
-        const h1 = wrapper.find('h1.coworkers-update__header--heading');
+        const h1 = wrapper.find('h1.coworkers-update__heading');
         expect(h1.exists()).toBe(true);
     })
 
     it('should have a button', () => {
         const wrapper = mount(CoworkersUpdate);
 
-        const button = wrapper.find('button.coworkers-update__header--button');
+        const button = wrapper.find('button.coworkers-update__button-edit');
         expect(button.exists()).toBe(true);
     })
 
@@ -42,19 +42,5 @@ describe('CoworkersOld Update', () => {
 
         const form = wrapper.find('form.coworkers-update__form');
         expect(form.exists()).toBe(true);
-    })
-
-    it('should have a label', () => {
-        const wrapper = mount(CoworkersUpdate);
-
-        const label = wrapper.find('label.coworkers-update__form--label');
-        expect(label.exists()).toBe(true);
-    })
-
-    it('should have an input', () => {
-        const wrapper = mount(CoworkersUpdate);
-
-        const input = wrapper.find('input.coworkers-update__form--input');
-        expect(input.exists()).toBe(true);
     })
 })
