b750368185d1b667b722421b5ecfd276115112df
julianacochenski <juliana.cochenski@gmail.com>
2022-02-08 10:59:37 -0300

    Refactoring index.ts, and renamed in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store/index';
+import store from './store';
 
 import 'c3/c3.min.css';
 
