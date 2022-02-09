cc0283196e1aee01f7839767f0f7ef4a8c071547
julianacochenski <juliana.cochenski@gmail.com>
2022-02-09 17:32:21 -0300

    Fix menu and dashboard after login.

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,28 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {computed, defineComponent} from "vue";
 import index from "./router";
 import Menu from "@/components/organisms/Menu/Menu.vue";
 import FooterUnit from "@/components/atoms/FooterUnit/FooterUnit.vue";
+import {useStore} from "vuex";
 
 export default defineComponent({
   name: 'App',
   components: { FooterUnit, Menu },
   router: index,
+  setup() {
+    const store = useStore();
+
+    const currentUser: {} = computed((): void => {
+      return store.state.auth.user;
+    });
+
+    return {
+      currentUser
+    }
+
+  }
 })
 </script>
 
@@ -30,7 +43,7 @@ export default defineComponent({
     grid-template-columns: 300px 1fr;
     grid-template-areas:
     "menu main"
-    "menu footer-unit";
+    "footer-unit footer-unit";
   }
 
   &__menu {
