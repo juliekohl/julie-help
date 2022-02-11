8366420fcab055c800c22f7ab481d4f184f23a67
julianacochenski <juliana.cochenski@gmail.com>
2022-02-11 17:44:24 -0300

    Add style on Dashboard.vue.

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="dashboard" v-if="currentUser">
-    <h1>Welcome {{ currentUser.name }}!</h1>
-    <div id="chart"></div>
+    <header class="dashboard__header">
+      <h1 class="dashboard__header-heading">Welcome <strong>{{ currentUser.name }}</strong>!</h1>
+    </header>
+    <div class="dashboard__chart" id="chart"></div>
   </div>
 </template>
 
@@ -28,7 +30,8 @@ export default defineComponent( {
             ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
 //            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
             ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 130, 340, 200, 500, 250, 350]
+            ['data2', 130, 340, 200, 500, 250, 350],
+            ['data3', 230, 480, 300, 600, 350, 450]
           ]
         },
         axis: {
@@ -54,5 +57,42 @@ export default defineComponent( {
 </script>
 
 <style lang="scss">
+.dashboard {
+  padding: 5rem;
+  background: var(--color-gray-blue);
+  box-shadow: var(--box-shadow-v2);
 
+  @include media('>=600') {
+    height: 100vh;
+    padding: 10rem;
+  }
+
+  &__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10rem;
+    padding: 5rem;
+
+    &-heading {
+      align-self: flex-start;
+      font-size: 13rem;
+      font-weight: 400;
+      color: var(--color-black);
+
+      @include media('>=600') {
+        font-size: 30rem;
+        font-weight: 500;
+      }
+
+      strong {
+        color:var(--color-secondary-40);
+      }
+    }
+  }
+  &__chart {
+    margin: 10rem;
+  }
+}
 </style>
