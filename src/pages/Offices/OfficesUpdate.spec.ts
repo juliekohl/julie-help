e334eb3a80f86d44af95c098e0303aab0bca76d4
julianacochenski <juliana.cochenski@gmail.com>
2022-02-20 15:13:32 -0300

    Fix test Offices*.spec.ts.

--- src/pages/Offices/OfficesUpdate.spec.ts
+++ src/pages/Offices/OfficesUpdate.spec.ts
@@ -1,4 +1,4 @@
-import {mount} from "@vue/test-utils";
+import {mount, RouterLinkStub} from "@vue/test-utils";
 import OfficesUpdate from "@/pages/Offices/OfficesUpdate.vue";
 
 jest.mock('vue-router', () => {
@@ -15,30 +15,38 @@ jest.mock('vue-router', () => {
     };
 });
 
+const options = {
+    global: {
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
+    }
+};
+
 describe('Offices Update', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(OfficesUpdate);
+        const wrapper = mount(OfficesUpdate, options);
 
         const officesUpdate = wrapper.find('div.offices-update');
         expect(officesUpdate.exists()).toBe(true);
     })
 
     it('should have an H1', () => {
-        const wrapper = mount(OfficesUpdate);
+        const wrapper = mount(OfficesUpdate, options);
 
         const h1 = wrapper.find('h1.offices-update__heading');
         expect(h1.exists()).toBe(true);
     })
 
     it('should have a button', () => {
-        const wrapper = mount(OfficesUpdate);
+        const wrapper = mount(OfficesUpdate, options);
 
         const button = wrapper.find('button.offices-update__button-edit');
         expect(button.exists()).toBe(true);
     })
 
     it('should have a form', () => {
-        const wrapper = mount(OfficesUpdate);
+        const wrapper = mount(OfficesUpdate, options);
 
         const form = wrapper.find('form.offices-update__form');
         expect(form.exists()).toBe(true);
