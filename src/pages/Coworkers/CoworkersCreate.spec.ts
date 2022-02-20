c82ab38417ee2ec4a76519c0977ea07ecb473e9a
julianacochenski <juliana.cochenski@gmail.com>
2022-02-20 15:04:30 -0300

    Fix test CoworkersCreate.spec.ts.

--- src/pages/Coworkers/CoworkersCreate.spec.ts
+++ src/pages/Coworkers/CoworkersCreate.spec.ts
@@ -1,44 +1,62 @@
-import {mount} from "@vue/test-utils";
+import {mount, RouterLinkStub} from "@vue/test-utils";
 import CoworkersCreate from "@/pages/Coworkers/CoworkersCreate.vue";
+
+const mountGlobal = {
+    stubs: {
+        RouterLink: RouterLinkStub
+    }
+};
 
 describe('New Coworkers', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const coworkersCreate = wrapper.find('div.coworkers-create');
         expect(coworkersCreate.exists()).toBe(true);
     })
 
     it('should have an H1 containing New Coworker', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const h1 = wrapper.find('h1.coworkers-create__heading');
         expect(h1.text()).toContain('Create Coworker');
     })
 
     it('should have add new coworker as button', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const button = wrapper.find('button.coworkers-create__button');
         expect(button.exists()).toBe(true);
     })
 
     it('should have the form', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const form = wrapper.find('Form.coworkers-create__form');
         expect(form.exists()).toBe(true);
     })
 
     it('should have label in the form', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const label = wrapper.find('label.coworkers-create__label');
         expect(label.exists()).toBe(true);
     })
 
     it('should have input in the form', () => {
-        const wrapper = mount(CoworkersCreate);
+        const wrapper = mount(CoworkersCreate, {
+            global: mountGlobal
+        });
 
         const input = wrapper.find('input.coworkers-create__input');
         expect(input.exists()).toBe(true);
