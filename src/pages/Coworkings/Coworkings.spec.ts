80777dc8fb127ed9d3d656d5bdffe2f05f5de552
julianacochenski <juliana.cochenski@gmail.com>
2022-02-20 15:13:12 -0300

    Fix test Coworkings.spec.ts.

--- src/pages/Coworkings/Coworkings.spec.ts
+++ src/pages/Coworkings/Coworkings.spec.ts
@@ -1,5 +1,6 @@
-import {mount} from "@vue/test-utils";
+import {mount, RouterLinkStub} from "@vue/test-utils";
 import CoworkingsUpdate from "@/pages/Coworkings/CoworkingsUpdate.vue";
+import CoworkersUpdate from "@/pages/Coworkers/CoworkersUpdate.vue";
 
 jest.mock('vue-router', () => {
     return {
@@ -15,30 +16,38 @@ jest.mock('vue-router', () => {
     };
 });
 
+const options = {
+    global: {
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
+    }
+};
+
 describe('Coworkings Update', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(CoworkingsUpdate);
+        const wrapper = mount(CoworkingsUpdate, options);
 
         const coworkingsUpdate = wrapper.find('div.coworkings-update');
         expect(coworkingsUpdate.exists()).toBe(true);
     })
 
     it('should have an H1', () => {
-        const wrapper = mount(CoworkingsUpdate);
+        const wrapper = mount(CoworkingsUpdate, options);
 
         const h1 = wrapper.find('h1.coworkings-update__heading');
         expect(h1.exists()).toBe(true);
     })
 
     it('should have a button', () => {
-        const wrapper = mount(CoworkingsUpdate);
+        const wrapper = mount(CoworkingsUpdate, options);
 
         const button = wrapper.find('button.coworkings-update__button-edit');
         expect(button.exists()).toBe(true);
     })
 
     it('should have a form', () => {
-        const wrapper = mount(CoworkingsUpdate);
+        const wrapper = mount(CoworkingsUpdate, options);
 
         const form = wrapper.find('form.coworkings-update__form');
         expect(form.exists()).toBe(true);
