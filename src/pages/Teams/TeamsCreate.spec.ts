13b811e521f7ce304cfb9c2bd34ff636a4f1ff30
julianacochenski <juliana.cochenski@gmail.com>
2022-02-20 15:13:52 -0300

    Fix test Teams*.spec.ts.

--- src/pages/Teams/TeamsCreate.spec.ts
+++ src/pages/Teams/TeamsCreate.spec.ts
@@ -1,44 +1,52 @@
-import {mount} from "@vue/test-utils";
+import {mount, RouterLinkStub} from "@vue/test-utils";
 import TeamsCreate from "@/pages/Teams/TeamsCreate.vue";
+
+const options = {
+    global: {
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
+    }
+};
 
 describe('New Teams', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const teamsCreate = wrapper.find('div.teams-create');
         expect(teamsCreate.exists()).toBe(true);
     })
 
     it('should have an H1 containing New Team', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const h1 = wrapper.find('h1.teams-create__heading');
         expect(h1.text()).toContain('Create Team');
     })
 
     it('should have add new team as button', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const button = wrapper.find('button.teams-create__button');
         expect(button.exists()).toBe(true);
     })
 
     it('should have the form', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const form = wrapper.find('Form.teams-create__form');
         expect(form.exists()).toBe(true);
     })
 
     it('should have label in the form', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const label = wrapper.find('label.teams-create__label');
         expect(label.exists()).toBe(true);
     })
 
     it('should have input in the form', () => {
-        const wrapper = mount(TeamsCreate);
+        const wrapper = mount(TeamsCreate, options);
 
         const input = wrapper.find('input.teams-create__input');
         expect(input.exists()).toBe(true);
--- src/pages/Teams/TeamsUpdate.spec.ts
+++ src/pages/Teams/TeamsUpdate.spec.ts
@@ -1,4 +1,4 @@
-import {mount} from "@vue/test-utils";
+import {mount, RouterLinkStub} from "@vue/test-utils";
 import TeamsUpdate from "@/pages/Teams/TeamsUpdate.vue";
 
 jest.mock('vue-router', () => {
@@ -15,30 +15,38 @@ jest.mock('vue-router', () => {
     };
 });
 
+const options = {
+    global: {
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
+    }
+};
+
 describe('Teams Update', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(TeamsUpdate);
+        const wrapper = mount(TeamsUpdate, options);
 
         const teamsUpdate = wrapper.find('div.teams-update');
         expect(teamsUpdate.exists()).toBe(true);
     })
 
     it('should have an H1', () => {
-        const wrapper = mount(TeamsUpdate);
+        const wrapper = mount(TeamsUpdate, options);
 
         const h1 = wrapper.find('h1.teams-update__heading');
         expect(h1.exists()).toBe(true);
     })
 
     it('should have a button', () => {
-        const wrapper = mount(TeamsUpdate);
+        const wrapper = mount(TeamsUpdate, options);
 
         const button = wrapper.find('button.teams-update__button-edit');
         expect(button.exists()).toBe(true);
     })
 
     it('should have a form', () => {
-        const wrapper = mount(TeamsUpdate);
+        const wrapper = mount(TeamsUpdate, options);
 
         const form = wrapper.find('form.teams-update__form');
         expect(form.exists()).toBe(true);
