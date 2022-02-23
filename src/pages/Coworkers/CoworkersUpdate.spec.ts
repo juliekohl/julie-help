c32a6bcda0013faa2b7854430fa2719e1d56e863
julianacochenski <juliana.cochenski@gmail.com>
2022-02-22 21:17:11 -0300

    Fix tests.

--- src/pages/Coworkers/CoworkersUpdate.spec.ts
+++ src/pages/Coworkers/CoworkersUpdate.spec.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {mount, RouterLinkStub} from "@vue/test-utils";
 import CoworkersUpdate from "@/pages/Coworkers/CoworkersUpdate.vue";
+import {createStore} from "vuex";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -19,9 +20,20 @@ jest.mock('vue-router', () => {
     };
 });
 
-const mountGlobal = {
-    stubs: {
-        RouterLink: RouterLinkStub
+const options = {
+    global: {
+        provide: {
+            store: createStore({
+                state: {
+                    auth: {
+                        user: {}
+                    }
+                }
+            })
+        },
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
     }
 };
 
@@ -33,36 +45,28 @@ describe('CoworkersOld Update', () => {
     })
 
     it('is an HTML tag div', () => {
-        const wrapper = mount(CoworkersUpdate, {
-            global: mountGlobal
-        });
+        const wrapper = mount(CoworkersUpdate, options);
 
         const coworkersUpdate = wrapper.find('div.coworkers-update');
         expect(coworkersUpdate.exists()).toBe(true);
     })
 
     it('should have an H1', () => {
-        const wrapper = mount(CoworkersUpdate, {
-            global: mountGlobal
-        });
+        const wrapper = mount(CoworkersUpdate, options);
 
         const h1 = wrapper.find('h1.coworkers-update__heading');
         expect(h1.exists()).toBe(true);
     })
 
     it('should have a button', () => {
-        const wrapper = mount(CoworkersUpdate, {
-            global: mountGlobal
-        });
+        const wrapper = mount(CoworkersUpdate, options);
 
         const button = wrapper.find('button.coworkers-update__button-edit');
         expect(button.exists()).toBe(true);
     })
 
     it('should have a form', () => {
-        const wrapper = mount(CoworkersUpdate, {
-            global: mountGlobal
-        });
+        const wrapper = mount(CoworkersUpdate, options);
 
         const form = wrapper.find('form.coworkers-update__form');
         expect(form.exists()).toBe(true);
