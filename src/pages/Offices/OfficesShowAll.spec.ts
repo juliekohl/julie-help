abcb2a5ab5a6dd94ead6955252da11077d3b7801
julianacochenski <juliana.cochenski@gmail.com>
2022-02-22 21:15:12 -0300

    Fix tests.

--- src/pages/Offices/OfficesShowAll.spec.ts
+++ src/pages/Offices/OfficesShowAll.spec.ts
@@ -1,17 +1,34 @@
 import {mount, RouterLinkStub} from "@vue/test-utils";
 import OfficesShowAll from "@/pages/Offices/OfficesShowAll.vue";
+import {createStore} from "vuex";
 
-const mountGlobal = {
-    stubs: {
-        RouterLink: RouterLinkStub
+jest.mock('vue-router', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+const options = {
+    global: {
+        provide: {
+            store: createStore({
+                state: {
+                    auth: {
+                        user: {}
+                    }
+                }
+            })
+        },
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
     }
 };
 
+
 describe('OfficesShowAll', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(OfficesShowAll, {
-            global: mountGlobal
-        });
+        const wrapper = mount(OfficesShowAll, options);
 
         const offices = wrapper.find('div.show-all');
         expect(offices.exists()).toBe(true);
--- src/pages/Plans/PlansShowAll.spec.ts
+++ src/pages/Plans/PlansShowAll.spec.ts
@@ -1,17 +1,33 @@
 import {mount, RouterLinkStub} from "@vue/test-utils";
 import PlansShowAll from "@/pages/Plans/PlansShowAll.vue";
+import {createStore} from "vuex";
 
-const mountGlobal = {
-    stubs: {
-        RouterLink: RouterLinkStub
+jest.mock('vue-router', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+const options = {
+    global: {
+        provide: {
+            store: createStore({
+                state: {
+                    auth: {
+                        user: {}
+                    }
+                }
+            })
+        },
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
     }
 };
 
 describe('PlansShowAll', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(PlansShowAll, {
-            global: mountGlobal
-        });
+        const wrapper = mount(PlansShowAll, options);
 
         const plans = wrapper.find('div.show-all');
         expect(plans.exists()).toBe(true);
