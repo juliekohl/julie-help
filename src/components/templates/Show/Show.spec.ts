fb329633d4a46b6f6c4d5ea4e3e0587f853fd166
julianacochenski <juliana.cochenski@gmail.com>
2022-02-22 21:59:43 -0300

    Simplify mountOptions.

--- src/components/templates/Show/Show.spec.ts
+++ src/components/templates/Show/Show.spec.ts
@@ -1,11 +1,11 @@
 import {mount} from "@vue/test-utils";
 import Show from "@/components/templates/Show/Show.vue";
-import {mountOptionsGlobal} from "../../../../tests/helpers";
+import {mountOptionsGlobal as global} from "../../../../tests/helpers";
 
 describe('Show', () => {
     it('show up title and extraTitle', () => {
         const wrapper = mount(Show, {
-            global: mountOptionsGlobal,
+            global,
             props: {
                 title: 'This Title',
                 extraTitle: 'This Extra Title',
