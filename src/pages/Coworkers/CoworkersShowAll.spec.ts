33b8affe77efabcaf3439f14f8d0a17ed0ff7535
julianacochenski <juliana.cochenski@gmail.com>
2022-02-22 21:15:12 -0300

    Fix tests.

--- src/pages/Coworkers/CoworkersShowAll.spec.ts
+++ src/pages/Coworkers/CoworkersShowAll.spec.ts
@@ -1,17 +1,33 @@
 import {mount, RouterLinkStub} from "@vue/test-utils";
 import CoworkersShowAll from "@/pages/Coworkers/CoworkersShowAll.vue";
+import {createStore} from "vuex";
 
-const mountGlobal = {
-    stubs: {
-        RouterLink: RouterLinkStub
+jest.mock('vue-router', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+const options = {
+    global: {
+        provide: {
+            store: createStore({
+                state: {
+                    auth: {
+                        user: {}
+                    }
+                }
+            })
+        },
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
     }
 };
 
 describe('CoworkersShowAll', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(CoworkersShowAll, {
-            global: mountGlobal
-        });
+        const wrapper = mount(CoworkersShowAll, options);
 
         const coworkers = wrapper.find('div.show-all');
         expect(coworkers.exists()).toBe(true);
--- src/pages/Teams/TeamsShowAll.spec.ts
+++ src/pages/Teams/TeamsShowAll.spec.ts
@@ -1,17 +1,33 @@
 import {mount, RouterLinkStub} from "@vue/test-utils";
 import TeamsShowAll from "@/pages/Teams/TeamsShowAll.vue";
+import {createStore} from "vuex";
 
-const mountGlobal = {
-    stubs: {
-        RouterLink: RouterLinkStub
+jest.mock('vue-router', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+const options = {
+    global: {
+        provide: {
+            store: createStore({
+                state: {
+                    auth: {
+                        user: {}
+                    }
+                }
+            })
+        },
+        stubs: {
+            RouterLink: RouterLinkStub
+        }
     }
 };
 
 describe('TeamsShowAll', () => {
     it('is an HTML tag div', () => {
-        const wrapper = mount(TeamsShowAll, {
-            global: mountGlobal
-        });
+        const wrapper = mount(TeamsShowAll, options);
 
         const teams = wrapper.find('div.show-all');
         expect(teams.exists()).toBe(true);
